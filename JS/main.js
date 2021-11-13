09830a0019c6d44ef3d04723a2a33aeaa2e69177
barbaramootian <barbaramootian@student.moringaschool.com>
2021-11-13 12:36:18 +0300

    added the function of akan

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,16 @@
-const name0fAkan = () =>{
-    var dateofbirth = document.getElementById("dateofbirth").value;
-    var gender = document.getElementById(gender).value;
+ function incorrect(day,month) {
+   if (day < 1 || day > 31 || month == 2 && day > 29){
+       alert ("Please enter a valid day")
+   }  
+ }
+ function Akan() {
+    var dateofbirth = parseInt(document.getElementById("day").value);
+    var monthofbirth = parseInt(document.getElementById("month").value);
+    var yearofbirth = parseInt(document.getElementById("year").value);
+    var gender = document.querySelector('input[name="Gender"]:checked').value
+
     var nameofmale = ["Kwasi"," Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi","Kwame"];
     var nameoffemale = [" Akosua", "Adwoa", "Abenaa","Akua"," Yaa"," Afua" ,"Ama"];
-    var dateoftheweeks =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-                         
-   if (dateofbirth.trim()=== "" || gender.trim() === ""){
-      document.getElementById("form").innerHTML = "Sorry, Please fill the form to know your Akan Name"
-      return false;
-   }else{
-       if(gender == "male"){
-           document.getElementById("name").innerHTML = ("Your Akan Name is" + " " +  nameofmale [day] );
-           document.getElementById("day").innerHTML = ("The day you are born is" + " " +  dateoftheweeks[day] );
-            return false;
-       }else{
-            document.getElementById("name").innerHTML = ("Your Akan Name is" + " " +  nameoffemale [day] );
-            document.getElementById("day").innerHTML = ("The day you are born is" + " " +  dateoftheweeks[day] );
-            return false;
-        }
-        return false;
-   }
-}
+    var dateoftheweeks =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];               
+ }
+ 
